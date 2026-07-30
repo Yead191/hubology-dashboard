de62df5f971f224352b55f07256da3537357ae9d
Yead191 <yead191@gmail.com>
2026-07-30 15:07:52 +0600

    added storee

--- src/app/AppProviders.tsx
+++ src/app/AppProviders.tsx
@@ -2,7 +2,6 @@ import type { ReactNode } from "react";
 import { ConfigProvider, App as AntdApp, theme as antdTheme } from "antd";
 import { Toaster } from "sonner";
 import { hubologyTheme } from "@/lib/theme";
-import { StoreProvider } from "@/features/store/StoreContext";
 import { MembershipProvider } from "@/features/membership/MembershipContext";
 import { ForumProvider } from "@/features/forum/ForumContext";
 import { IFundAyitiProvider } from "@/features/ifundayiti/IFundAyitiContext";
@@ -11,27 +10,25 @@ export function AppProviders({ children }: { children: ReactNode }) {
   return (
     <ConfigProvider theme={{ algorithm: antdTheme.darkAlgorithm, ...hubologyTheme }}>
       <AntdApp>
-        <StoreProvider>
-          <MembershipProvider>
-            <ForumProvider>
-              <IFundAyitiProvider>
-                {children}
-                <Toaster
-                  theme="dark"
-                  position="top-right"
-                  richColors
-                  toastOptions={{
-                    style: {
-                      background: "#141737",
-                      border: "1px solid #23274f",
-                      color: "#eef0fb",
-                    },
-                  }}
-                />
-              </IFundAyitiProvider>
-            </ForumProvider>
-          </MembershipProvider>
-        </StoreProvider>
+        <MembershipProvider>
+          <ForumProvider>
+            <IFundAyitiProvider>
+              {children}
+              <Toaster
+                theme="dark"
+                position="top-right"
+                richColors
+                toastOptions={{
+                  style: {
+                    background: "#141737",
+                    border: "1px solid #23274f",
+                    color: "#eef0fb",
+                  },
+                }}
+              />
+            </IFundAyitiProvider>
+          </ForumProvider>
+        </MembershipProvider>
       </AntdApp>
     </ConfigProvider>
   );
